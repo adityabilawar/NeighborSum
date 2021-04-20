ba6d11b84f8118e2d823f7a40d723a2859efd118
Aditya Bilawar <61608353+adityabilawar@users.noreply.github.com>
2021-04-20 17:22:52 -0400

    Add files via upload

---
+++ NeighborSum.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class NeighborSum {
+
+	private int[][] chart = { 
+			{ 1, 2, 3, 4, 5 }, 
+			{ 6, 7, 8, 9, 0 }, 
+			{ 6, 7, 1, 2, 5 }, 
+			{ 6, 7, 8, 9, 0 },
+			{ 5, 4, 3, 2, 1 }, 
+			{ 0, 0, 0, 0, 0 } };
+
+
+	private int sum(int row, int col) {
+		
+		int total = 0;
+		for (int r = row-1; r <= row + 1; r++) {
+            for (int c = col -1 ; c <= col + 1; c++) {
+                if ((r >= 0 && r < chart.length) && (c >= 0 && c < chart[0].length)) {
+                    total+=chart[r][c];
+                }
+            }
+        }
+        return total - chart[row][col];
+
+
+
+	}
+
+
+	public void go() throws Exception {
+		Scanner file = new Scanner(new File("NeighborSumChart.dat"));
+		int pairs = file.nextInt();
+		for (int i = 0; i < pairs; i++) {
+			int r = file.nextInt();
+			int c = file.nextInt();
+			System.out.println("The sum of " + r + "," + c + " is " + sum(r, c));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		NeighborSum x = new NeighborSum();
+		x.go();
+	}
+}
